92eae326cf7e3d3019d854f518c668a6c5fba9e9
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-03-23 15:59:26 +0100

    :construction: just to help for the develop branch

--- amp-front/src/router/routes.js
+++ amp-front/src/router/routes.js
@@ -4,7 +4,7 @@ const routes = [
     path: '/',
     component: () => import('layouts/MainLayout.vue'),
     children: [
-      { path: '', component: () => import('pages/Index.vue') },
+      { path: '', name: 'home', component: () => import('pages/Index.vue') },
       { path: '/info', component: () => import('pages/Info.vue') },
       { path: '/login', component: () => import('pages/Login.vue') },
       { path: '/disease', component: () => import('pages/Disease.vue') }
