9db6a35da7a9f5f1cf85002e92fd13bcffbb3533
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-03-30 10:36:14 +0200

    :construction: Did i forget to add the files ?...

---
+++ amp-front/src/pages/DetailsDisease.vue
@@ -0,0 +1,47 @@
+<template lang="">
+  <div>
+    {{message}} :
+    {{disease.name_disease}}
+  </div>
+</template>
+<script>
+import * as axios from 'axios'
+// a mettre dans les variable
+const apiAddr = 'http://localhost:2395/api'
+
+export default {
+  name: 'DetailDisease',
+  props: {
+    disease: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {
+      message: 'page detals for disease',
+      diseases: undefined,
+      diseaseCard: {
+        name: '',
+        description: '',
+        isVaccine: false
+      }
+    }
+  },
+  methods: {
+    getDisease () {
+      axios.get(`${apiAddr}/diseases`).then(elem => { this.disease = elem.data })
+      console.log('les maladie chargé')
+      console.log(this.diseases)
+    },
+    async loadOneDisease (id) {
+      const ret = await axios.get(`${apiAddr}/diseases/${id}`)
+      console.log(ret)
+    }
+  },
+  created () {
+    this.loadOneDisease(this.diseases.id)
+  }
+}
+</script>
+<style lang=""></style>
