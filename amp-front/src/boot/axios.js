c832219fb621a7d415af2284f651f8682fd33fe3
Unknow46 <nathan.sens@hotmail.fr>
2021-04-06 16:39:12 +0200

    :construction: making page research disease by symptoms

--- amp-front/src/boot/axios.js
+++ amp-front/src/boot/axios.js
@@ -5,15 +5,15 @@ let axiosInstance
 
 if (process.env.DEBUGGING) {
   axiosInstance = axios.create({
-    baseURL: 'http://localhost:2395/api'
+    baseURL: ''
     /** http://localhost:8000/api/v1 **/
-    /** http://localhost:2395/api' */
   })
 } else {
   axiosInstance = axios.create({
     baseURL: 'https://amp-back.herokuapp.com/api'
   })
 }
+
 Vue.prototype.$axios = axiosInstance
 
 export { axiosInstance }
--- amp-front/src/router/routes.js
+++ amp-front/src/router/routes.js
@@ -6,15 +6,17 @@ const routes = [
     children: [
       { path: '', name: 'home', component: () => import('pages/Index.vue'), meta: { requiresAuth: true } },
       { path: '/info', name: 'info', component: () => import('pages/Info.vue') },
-      { path: '/places', name: 'places', component: () => import('pages/Places.vue') },
-      { path: '/details_place', name: 'details_places', component: () => import('pages/DetailsPlace.vue') },
-      { path: '/disease', name: 'disease', component: () => import('pages/Disease.vue') },
+      { path: '/places', name: 'places', component: () => import('pages/Places.vue'), meta: { requiresAuth: true } },
+      { path: '/details_place', name: 'details_places', component: () => import('pages/DetailsPlace.vue'), meta: { requiresAuth: true } },
+      { path: '/disease', name: 'disease', component: () => import('pages/Disease.vue'), meta: { requiresAuth: true } },
       {
         path: '/disease/:id',
         name: 'disease_details',
         component: () => import('pages/DeseasePageDetails.vue'),
+        meta: { requiresAuth: true },
         props: r => ({ id: r.params.id })
-      }
+      },
+      { path: '/symptoms', name: 'symptoms', component: () => import('pages/Symptoms'), meta: { requiresAuth: true } }
     ]
   },
   {
