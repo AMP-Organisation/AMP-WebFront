6ce748d29172b82845fa628bcdd1432ef9fd3f2f
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-04-07 16:50:13 +0200

    :construction: working on component creation : Treatment component
    I have to add the date and improve the ui

---
+++ amp-front/src/components/Treatment.vue
@@ -0,0 +1,36 @@
+<template lang="">
+  <div>
+    <q-card class="my-card">
+      <q-card-section class="bg-teal text-white">
+        <div class="text-h6">{{ name }}</div>
+        <div class="text-subtitle2">sous titre</div>
+      </q-card-section>
+
+      <q-card-actions vertical align="right">
+        <p class="text-body1">{{ description }}</p>
+      </q-card-actions>
+    </q-card>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Treatment',
+  props: {
+    name: {
+      type: String,
+      default: 'un nom par defaut'
+    },
+    description: {
+      type: String,
+      default: 'ue description par defaut et si c\'est par defaut c\'est qu\'il y a eu un probleme'
+    }
+  },
+  data () {
+    return {
+    }
+  },
+  created () {
+    console.log('composant traitement créé')
+  }
+}
+</script>
--- amp-front/src/pages/OptionOne.vue
+++ amp-front/src/pages/OptionOne.vue
@@ -1,15 +1,42 @@
 <template lang="">
   <div>
     <h3>{{ intro }}</h3>
+    <div class="q-mr-xl q-ml-xl q-mb-md">
+      <Treatment class="q-mb-md" v-for="tre in  traitement" :key="tre.nom" :name='tre.nom' :description="tre.description" ></Treatment>
+    </div>
   </div>
 </template>
 <script>
+import Treatment from 'src/components/Treatment.vue'
+import { date } from 'quasar'
+
 export default {
   name: 'option_one',
+  components: {
+    Treatment
+  },
   data () {
     return {
-      intro: 'page de l\'option 1'
+      intro: 'page de l\'option 1',
+      dt: undefined,
+      traitement: [
+        {
+          nom: 'un nom',
+          description: 'une description au premier'
+        },
+        {
+          nom: 'deux',
+          description: 'une description au second'
+        }
+      ]
     }
+  },
+  created () {
+    this.dt = new Date()
+    console.log('opetion un créé')
+    console.log(this.dt)
+    const formatedDate = date.formatDate(this.dt, 'DD MMMM YYYY  HH:mm')
+    console.log(formatedDate)
   }
 }
 </script>
