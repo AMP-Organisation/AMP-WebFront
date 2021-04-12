65c4d358a478548111bb489108dbe985e81226e6
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-04-12 11:29:52 +0200

    :construction: I continue the treatment components

--- amp-front/src/components/Treatment.vue
+++ amp-front/src/components/Treatment.vue
@@ -4,6 +4,9 @@
       <q-card-section class="bg-teal text-white">
         <div class="text-h6">{{ name }}</div>
         <div class="text-subtitle2">sous titre</div>
+        <div>début du traitement :{{ dateBegin | formatTheDate }}</div>
+        <div v-if="dateEnd != null">{{ dateEnd | formatTheDate }}</div>
+        <div v-else>traitement en cours</div>
       </q-card-section>
 
       <q-card-actions vertical align="right">
@@ -13,6 +16,7 @@
   </div>
 </template>
 <script>
+import { date } from 'quasar'
 export default {
   name: 'Treatment',
   props: {
@@ -23,6 +27,21 @@ export default {
     description: {
       type: String,
       default: 'ue description par defaut et si c\'est par defaut c\'est qu\'il y a eu un probleme'
+    },
+    dateBegin: {
+      type: Date,
+      default: new Date()
+    },
+    dateEnd: {
+      type: Date,
+      default: null
+    }
+  },
+  filters: {
+    formatTheDate: function (dt) {
+      console.log(`je formate : ${dt}`)
+      const formattedString = date.formatDate(dt, 'YY-MMMM-DD')
+      return formattedString
     }
   },
   data () {
@@ -31,6 +50,12 @@ export default {
   },
   created () {
     console.log('composant traitement créé')
+    console.log(`la date : ${this.dateBegin}`)
+    if (this.dateEnd == null) {
+      console.log('c\'est bien null')
+    } else {
+      console.log('il y a bien une date de fin')
+    }
   }
 }
 </script>
--- amp-front/src/pages/OptionOne.vue
+++ amp-front/src/pages/OptionOne.vue
@@ -2,7 +2,11 @@
   <div>
     <h3>{{ intro }}</h3>
     <div class="q-mr-xl q-ml-xl q-mb-md">
-      <Treatment class="q-mb-md" v-for="tre in  traitement" :key="tre.nom" :name='tre.nom' :description="tre.description" ></Treatment>
+      <Treatment class="q-mb-md" v-for="tre in  traitement"
+      :key="tre.nom"
+      :name='tre.nom'
+      :description="tre.description"
+      :dateBegin="tre.dateBegin"/>
     </div>
   </div>
 </template>
@@ -22,21 +26,32 @@ export default {
       traitement: [
         {
           nom: 'un nom',
-          description: 'une description au premier'
+          description: 'une description au premier',
+          dateBegin: new Date(),
+          dateEnd: undefined
         },
         {
           nom: 'deux',
-          description: 'une description au second'
+          description: 'une description au second',
+          dateBegin: new Date(),
+          dateEnd: undefined
+        },
+        {
+          nom: 'un traitement fini',
+          description: 'une description du traitement terminé',
+          dateBegin: new Date(2017, 2, 7),
+          dateEnd: new Date(2018, 2, 7)
         }
       ]
     }
   },
   created () {
     this.dt = new Date()
-    console.log('opetion un créé')
+    console.log('option un créé')
     console.log(this.dt)
     const formatedDate = date.formatDate(this.dt, 'DD MMMM YYYY  HH:mm')
     console.log(formatedDate)
+    console.log(this.traitement)
   }
 }
 </script>
