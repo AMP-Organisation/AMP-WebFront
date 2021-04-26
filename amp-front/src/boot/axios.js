b266eda05bde2fc8e9560fd8031909da44282172
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-04-26 13:52:02 +0200

    :memo:Comment line for Axios

--- amp-front/src/boot/axios.js
+++ amp-front/src/boot/axios.js
@@ -5,8 +5,9 @@ let axiosInstance
 
 if (process.env.DEBUGGING) {
   axiosInstance = axios.create({
-    baseURL: 'http://localhost:2395/api/'
+    baseURL: ''
     /** http://localhost:8000/api/v1 **/
+    /** http://localhost:2395/api/ **/
   })
 } else {
   axiosInstance = axios.create({
