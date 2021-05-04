886eb4a5e0d1c0122903b0c5768c0724430e8d73
Baptiste Bertrand-Rapello <baptiste2394@gmail.com>
2021-05-04 17:04:12 +0200

    :constrcution: I am working on IMC page mostly
    I have the list with the graphe and I added some option-group and expanded panel : very nice :-)

--- amp-front/src/pages/FollowUp.vue
+++ amp-front/src/pages/FollowUp.vue
@@ -1,44 +1,213 @@
 <template>
   <div>
-    <IconAndTitle :title="intro" :color="'teal-4'" />
-    <div>
-      <q-card class="q-ml-md q-mr-md ">
+    <IconAndTitle :title="intro" :color="'teal-4'" :icon="'moving'" />
+    <div class=" q-pb-xl">
+      <q-card class="q-ml-md q-mr-md q-mb-md">
+        <q-expansion-item
+        expand-separator
+        v-model="expanded_imc"
+        icon="monitor_weight"
+        label="Follow-up IMC"
+        :expand-icon-class="'text-teal-4'"
+        :header-class="'text-h5'"
+        >
+        <!-- premiere partie de la carte avec le titre et l'objectif de poid -->
         <q-card-section>
-          <p>toto</p>
+          <div class="row">
+            <div class="col">
+              <div class="col-1">
+                <div v-if="hasReachedObjective()">
+                  <div class="text-h3 text-red-9" color="red-9">
+                    {{ objective }}
+                  </div>
+                </div>
+                <div v-else>
+                  <div class="text-h3 text-teal-9" color="teal-9">
+                    {{ objective }}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </q-card-section>
+
         <q-separator />
+        <!-- le line chart CAD le graphique -->
         <q-card-section>
-          <q-list
-            bordered
-            separator
-            v-for="wdata in weekData"
-            v-bind:key="wdata.date"
-          >
-            <q-item clickable v-ripple>
-              <q-item-section>
-                <q-item-label
-                  >Le : {{ wdata.date | formatTheDate }}</q-item-label
-                >
-              </q-item-section>
-              <q-item-section>Poids de : {{ wdata.weight }} </q-item-section>
-            </q-item>
-          </q-list>
+          <q-tab-panels v-model="panel" animated class="shadow-2 rounded-borders">
+            <q-tab-panel name="week">
+              <LineChart
+                v-if="datatab"
+                :chartData="dataChart"
+                :options="dataOption"
+                :which="true"
+                :duration="'week'"
+                :dataToCompute="dataTabdeci"
+              />
+            </q-tab-panel>
+            <q-tab-panel name="month">
+              <LineChart
+                v-if="datatab"
+                :chartData="dataChart"
+                :options="dataOption"
+                :which="true"
+                :duration="'week'"
+                :dataToCompute="dataTabdeci"
+              />
+            </q-tab-panel>
+          </q-tab-panels>
         </q-card-section>
+        <q-card-section >
+          <div class="row">
+            <div class="col">
+            <q-option-group
+                      v-model="panel"
+                      inline
+                      :options="[
+                        { label: 'Week', value: 'week' },
+                        { label: 'Month', value: 'month' },
+                        { label: 'Semester', value: 'semester' },
+                        { label: 'Year', value: 'year' }
+                      ]"
+                    />
+                    </div>
+          </div>
+        </q-card-section>
+        <!-- la deuxieme partie avec la liste et la carte a coté -->
         <q-card-section>
-          <LineChart
-            v-if="datatab"
-            :chartData="dataChart"
-            :options="dataOption"
-            :which="true"
-            :duration="'week'"
-            :dataToCompute="dataTabdeci"
-          />
+          <div class="row">
+            <!-- la section a droite de la liste -->
+            <div class="col-6">
+              <!-- <q-card class="q-ml-md q-mr-md">
+                <p>bonjour</p>
+              </q-card> -->
+                <div class="q-pa-md">
+                  <div class="" >
+                    <q-option-group
+                      v-model="panel"
+                      inline
+                      :options="[
+                        { label: 'Week', value: 'week' },
+                        { label: 'Month', value: 'month' },
+                        { label: 'Semester', value: 'semester' },
+                        { label: 'Year', value: 'year' }
+                      ]"
+                    />
+                    <q-tab-panels v-model="panel" animated class="shadow-2 rounded-borders">
+                      <!-- par semaine -->
+                      <q-tab-panel name="week">
+                        <div class="text-h5 q-mb-sm">
+                          Week
+                        </div>
+                        <q-list
+                          bordered
+                          separator
+                          v-for="wdata in weekData"
+                          v-bind:key="wdata.date">
+                          <q-item clickable v-ripple>
+                            <q-item-section class="col-1">
+                              <div v-if="loseWeight">
+                                <q-icon v-if="progression(wdata.date)" name="south_east" :size="'xl'" :color="'teal-9'">
+                                  <q-tooltip>You lost some weight</q-tooltip>
+                                </q-icon>
+                                <q-icon v-else name="north_east" :size="'xl'" :color="'red-9'">
+                                  <q-tooltip>You gain some weight</q-tooltip>
+                                </q-icon>
+                              </div>
+                            </q-item-section>
+                            <q-item-section class="col">
+                              <q-item-label>Le : {{ wdata.date | formatTheDate }}</q-item-label>
+                              <q-item-label>Poids de : {{ wdata.weight }} </q-item-label>
+                            </q-item-section>
+                          </q-item>
+                        </q-list>
+                      </q-tab-panel>
+                      <!-- par mois -->
+                      <q-tab-panel name="month">
+                        <div class="text-h6">Month</div>
+                          <q-list
+                            bordered
+                            separator
+                            v-for="elemData in monthData"
+                            v-bind:key="elemData.date">
+                            <q-item clickable v-ripple>
+                              <q-item-section class="col-1">
+                                <div v-if="loseWeight">
+                                  <q-icon v-if="progression(elemData.date)" name="south_east" :size="'xl'" :color="'teal-9'">
+                                    <q-tooltip>You lost some weight</q-tooltip>
+                                  </q-icon>
+                                  <q-icon v-else name="north_east" :size="'xl'" :color="'red-9'">
+                                    <q-tooltip>You gain some weight</q-tooltip>
+                                  </q-icon>
+                                </div>
+                              </q-item-section>
+                              <q-item-section class="col">
+                                <q-item-label>Le : {{ elemData.date | formatTheDate }}</q-item-label>
+                                <q-item-label>Poids de : {{ elemData.weight }} </q-item-label>
+                              </q-item-section>
+                            </q-item>
+                          </q-list>
+                      </q-tab-panel>
+                      <!-- par semestre (enfin je ne sais pas si on le gardera celui ci) -->
+                      <q-tab-panel name="semester">
+                        <div class="text-h6">Movies</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </q-tab-panel>
+                      <q-tab-panel name="year">
+                        <div class="text-h6">Movies</div>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                      </q-tab-panel>
+                    </q-tab-panels>
+                  </div>
+                </div>
+            </div>
+            <!-- la liste -->
+            <div class="col-6">
+              <q-list
+                bordered
+                separator
+                v-for="wdata in weekData"
+                v-bind:key="wdata.date">
+                <q-item clickable v-ripple>
+                  <q-item-section class="col-1">
+                    <div v-if="loseWeight">
+                      <q-icon v-if="progression(wdata.date)" name="south_east" :size="'xl'" :color="'teal-9'">
+                        <q-tooltip>You lost some weight</q-tooltip>
+                      </q-icon>
+                      <q-icon v-else name="north_east" :size="'xl'" :color="'red-9'">
+                        <q-tooltip>You gain some weight</q-tooltip>
+                      </q-icon>
+                    </div>
+                  </q-item-section>
+                  <q-item-section class="col">
+                    <q-item-label>Le : {{ wdata.date | formatTheDate }}</q-item-label>
+                    <q-item-label>Poids de : {{ wdata.weight }} </q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </div>
+          </div>
         </q-card-section>
+
         <q-card-section>
           <div>
             <p>toto</p>
           </div>
         </q-card-section>
+        </q-expansion-item>
+      </q-card>
+      <!-- le deuxieme expansion item pour un autre suivi -->
+      <q-card class="q-ml-md q-mr-md q-mt-md">
+        <q-expansion-item
+        expand-separator
+        icon="shuffle"
+        label="Follow-up je ne sais pas quoi">
+        <q-card-section>
+          <q-label>
+            toto
+          </q-label>
+        </q-card-section>
+        </q-expansion-item>
       </q-card>
     </div>
   </div>
@@ -53,70 +222,122 @@ export default {
   name: 'followup_page',
   data () {
     return {
-      intro: 'Follow Up Page For IMC',
+      intro: 'Follow Up Page',
+      expanded_imc: true,
       test: {
         toto: 'titi'
       },
       charttest: undefined,
+      size: 1.80,
+      objective: 70,
+      // loseWeight est un booleen qui désigne le fait de vouloir perdre du poid, ou d'en gagner
+      loseWeight: true,
+      panel: 'month',
       dataTabdeci: [75.4, 75.1, 74.8, 74.3, 75.1, 75.2, 75.1],
+      lastData: [
+        {
+          date: '2021-04-030T15:01:09.538Z',
+          weight: 75.1,
+          imc: 0
+        }
+      ],
       weekData: [
         {
           date: '2021-04-030T15:01:09.538Z',
-          weight: 75.1
+          weight: 75.1,
+          imc: 0
         },
         {
           date: '2021-04-029T15:01:09.538Z',
-          weight: 75.2
+          weight: 75.2,
+          imc: 0
         },
         {
           date: '2021-04-028T15:01:09.538Z',
-          weight: 75.1
+          weight: 75.1,
+          imc: 0
         },
         {
           date: '2021-04-027T15:01:09.538Z',
-          weight: 74.3
+          weight: 74.3,
+          imc: 0
         },
         {
           date: '2021-04-026T15:01:09.538Z',
-          weight: 74.8
+          weight: 74.8,
+          imc: 0
         },
         {
           date: '2021-04-025T15:01:09.538Z',
-          weight: 75.1
+          weight: 75.1,
+          imc: 0
         },
         {
           date: '2021-04-024T15:01:09.538Z',
-          weight: 75.4
+          weight: 75.4,
+          imc: 0
         }
       ],
       monthData: [
         {
           date: '2021-02-09T15:01:09.538Z',
-          weight: 75
+          weight: 75,
+          imc: 0
         },
         {
-          date: '2021-02-09T15:01:09.538Z',
-          weight: 75
+          date: '2021-02-08T15:01:09.538Z',
+          weight: 75,
+          imc: 0
         },
         {
-          date: '2021-02-09T15:01:09.538Z',
-          weight: 75
+          date: '2021-02-07T15:01:09.538Z',
+          weight: 75,
+          imc: 0
         },
         {
-          date: '2021-02-09T15:01:09.538Z',
-          weight: 75
+          date: '2021-02-06T15:01:09.538Z',
+          weight: 75,
+          imc: 0
         },
         {
-          date: '2021-02-09T15:01:09.538Z',
-          weight: 75
+          date: '2021-02-05T15:01:09.538Z',
+          weight: 75,
+          imc: 0
         },
         {
-          date: '2021-02-09T15:01:09.538Z',
-          weight: 75
+          date: '2021-02-04T15:01:09.538Z',
+          weight: 75,
+          imc: 0
         },
         {
-          date: '2021-02-09T15:01:09.538Z',
-          weight: 75
+          date: '2021-02-03T15:01:09.538Z',
+          weight: 75,
+          imc: 0
+        },
+        {
+          date: '2021-02-02T15:01:09.538Z',
+          weight: 75,
+          imc: 0
+        },
+        {
+          date: '2021-02-01T15:01:09.538Z',
+          weight: 75,
+          imc: 0
+        },
+        {
+          date: '2021-01-31T15:01:09.538Z',
+          weight: 75,
+          imc: 0
+        },
+        {
+          date: '2021-02-30T15:01:09.538Z',
+          weight: 75,
+          imc: 0
+        },
+        {
+          date: '2021-02-29T15:01:09.538Z',
+          weight: 75,
+          imc: 0
         }
       ],
       dataOption: {
@@ -164,9 +385,20 @@ export default {
       return formatedDate
     }
   },
+  methods: {
+    hasReachedObjective () {
+      if (this.lastData.weight <= this.objective) {
+        return true
+      } else {
+        return false
+      }
+    },
+    progression () {
+      return true
+    }
+  },
   created () {
     console.log('dans la page follow up')
-    console.log(this.test)
     console.log(this.dataChart)
     this.weekData.filter(elem => {
       this.datatab.push(elem.weight)
